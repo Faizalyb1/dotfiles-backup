22e7615ff62f462f3a324454f1fa4f5213b7fd61
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-19 03:25:57 +0530

    Delete fonts.sh

--- install-scripts/fonts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-printf '\033c'
-sudo pacman -Syu --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
-echo "fonts done"
