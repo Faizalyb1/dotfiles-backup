63d634083ed1f1e762be1b7a06cb6b57239a4d71
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-22 15:29:58 +0530

    Update post-install.sh

--- install-scripts/post-install.sh
+++ install-scripts/post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf '\033c'
-sudo pacman -Sy xorg-server xorg-xinit pacman-contrib rclone aria2 scrot simplescreenrecorder fzf lxappearance firefox xterm mtools dosfstools xorg-xrandr gnome-disk-utility udisks2 spacefm xorg-xbacklight xdg-utils xdg-user-dirs xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu gtk2 gtk3 xf86-video-ati gparted mpv qt5ct gvfs ntp-dinit mupdf file-roller 
+sudo pacman -Sy xorg-server xorg-xinit pacman-contrib rclone aria2 yt-dlp p7zip ffmpeg scrot simplescreenrecorder fzf lxappearance firefox xterm mtools dosfstools xorg-xrandr gnome-disk-utility udisks2 xorg-xbacklight xdg-utils xdg-user-dirs xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu gtk2 gtk3 xf86-video-ati gparted mpv qt5ct gvfs ntp-dinit mupdf file-roller 
 clear
 sleep 2s
 xdg-user-dirs-update
